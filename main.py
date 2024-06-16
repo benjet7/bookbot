3da148b7781a8aa5ac454c78bb388c727c650dd3
bgenest <benjet7@hotmail.com>
2024-06-16 18:05:11 -0400

    update main.py to modify the word count function

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        words = file_contents.split()
-        #print(words)
-        #print(file_contents)
+    path = "books/frankenstein.txt"
+    file_content = get_file_content(path)
+    words = file_content.split()
+    word_count = len(words)
+    print(f"{word_count} words found in document")
 
-        def counting_words(x):
-            word_count = 0
-            for word in x:
-                word_count += 1
-            return word_count
-
-        print(counting_words(words))
-
-
-
+def get_file_content(path):
+    with open(path) as f:
+        return f.read()
 
 main()
