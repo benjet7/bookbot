87a9fdb79daa42ca9781f8878eb11e161c11d07f
bgenest <benjet7@hotmail.com>
2024-06-16 18:36:05 -0400

    update main.py to add the count characters function

--- main.py
+++ main.py
@@ -3,10 +3,21 @@ def main():
     file_content = get_file_content(path)
     words = file_content.split()
     word_count = len(words)
+    lowercase_string = file_content.lower()
     print(f"{word_count} words found in document")
+    print(count_characters(lowercase_string))
+    
 
 def get_file_content(path):
     with open(path) as f:
         return f.read()
+
+def count_characters(lowercase_string):
+    character_count = {}
+    ascii_lowercase = "abcdefghijklmnopqrstuvwxyz"
+    for letter in ascii_lowercase:
+        character_count[letter] = lowercase_string.count(letter)
+    return character_count
+
 
 main()
