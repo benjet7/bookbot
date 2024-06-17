6318f4e57e11db61f928ad239465d5c3db11baa3
bgenest <benjet7@hotmail.com>
2024-06-16 22:13:45 -0400

    update main.py to print a report of the count character function in descending order

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ def main():
     words = file_content.split()
     word_count = len(words)
     lowercase_string = file_content.lower()
+    ch_results = count_characters(lowercase_string)
+    print(f"--- Begin report of {path} ---")
     print(f"{word_count} words found in document")
-    print(count_characters(lowercase_string))
+    print()
+    print_charater_count_to_console(ch_results)
+    print("--- End report ---")
+
+    #print(count_characters(lowercase_string))
     
 
 def get_file_content(path):
@@ -19,5 +25,9 @@ def count_characters(lowercase_string):
         character_count[letter] = lowercase_string.count(letter)
     return character_count
 
+def print_charater_count_to_console(ch_results):
+    ch_sorted = dict(sorted(ch_results.items(), key=lambda x: x[1], reverse=True))
+    for ch in ch_sorted:
+        print(f"The '{ch}' character was found {ch_sorted[ch]} times")
 
 main()
